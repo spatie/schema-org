d094d5e32ce41519b328b5a475e1403a8b85be57
Alex Vanderbist <alex.vanderbist@gmail.com>
2017-02-17 16:25:30 +0100

    StyleCI fixes

--- generator/Writer/Filters.php
+++ generator/Writer/Filters.php
@@ -7,11 +7,11 @@ class Filters
     public static function doc($text, array $options = []): string
     {
         $indentation = $options[0] ?? 0;
-        $docblockPrefix = str_repeat(' ', $indentation * 4) . ' * ';
+        $docblockPrefix = str_repeat(' ', $indentation * 4).' * ';
 
         $lineLength = 80 - 3 - ($indentation * 4);
 
-        $text = str_replace("<br />", '', $text);
+        $text = str_replace('<br />', '', $text);
 
         $text = preg_replace('/(\n|\\\n)/', "\n".$docblockPrefix, $text);
 
