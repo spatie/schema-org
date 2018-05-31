6eee5024694ddd4047c8a2e5bcdc0b994df2f9d0
Tom Witkowski <dev.gummibeer@gmail.com>
2018-05-31 09:25:13 +0200

    regenerate with multiple parents src/TieAction.php

--- src/TieAction.php
+++ src/TieAction.php
@@ -6,7 +6,9 @@ namespace Spatie\SchemaOrg;
  * The act of reaching a draw in a competitive activity.
  *
  * @see http://schema.org/TieAction
+ *
+ * @mixin AchieveAction
  */
-class TieAction extends AchieveAction
+class TieAction
 {
 }
