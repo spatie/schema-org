3939424d4c7c9325ac1b1db488238b9ad7ac5d9f
Tom Witkowski <dev.gummibeer@gmail.com>
2018-05-31 09:25:03 +0200

    regenerate with multiple parents src/SelfStorage.php

--- src/SelfStorage.php
+++ src/SelfStorage.php
@@ -6,7 +6,9 @@ namespace Spatie\SchemaOrg;
  * A self-storage facility.
  *
  * @see http://schema.org/SelfStorage
+ *
+ * @mixin LocalBusiness
  */
-class SelfStorage extends LocalBusiness
+class SelfStorage
 {
 }
