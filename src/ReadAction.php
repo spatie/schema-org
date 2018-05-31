eaf9401dad590eebec9272ddf0b917d9a93e0267
Tom Witkowski <dev.gummibeer@gmail.com>
2018-05-31 09:24:56 +0200

    regenerate with multiple parents src/ReadAction.php

--- src/ReadAction.php
+++ src/ReadAction.php
@@ -6,7 +6,9 @@ namespace Spatie\SchemaOrg;
  * The act of consuming written content.
  *
  * @see http://schema.org/ReadAction
+ *
+ * @mixin ConsumeAction
  */
-class ReadAction extends ConsumeAction
+class ReadAction
 {
 }
