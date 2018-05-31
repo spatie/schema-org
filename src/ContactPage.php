1e2cf112986fcf54e2c5e67ce984e51ed2a6feec
Tom Witkowski <dev.gummibeer@gmail.com>
2018-05-31 09:23:58 +0200

    regenerate with multiple parents src/ContactPage.php

--- src/ContactPage.php
+++ src/ContactPage.php
@@ -6,7 +6,9 @@ namespace Spatie\SchemaOrg;
  * Web page type: Contact page.
  *
  * @see http://schema.org/ContactPage
+ *
+ * @mixin WebPage
  */
-class ContactPage extends WebPage
+class ContactPage
 {
 }
