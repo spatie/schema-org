5931deee6e984c5095e18661f90d1fcb7e16ac00
Tom Witkowski <dev.gummibeer@gmail.com>
2018-05-31 09:23:46 +0200

    regenerate with multiple parents src/BookSeries.php

--- src/BookSeries.php
+++ src/BookSeries.php
@@ -6,7 +6,9 @@ namespace Spatie\SchemaOrg;
  * A series of books. Included books can be indicated with the hasPart property.
  *
  * @see http://schema.org/BookSeries
+ *
+ * @mixin CreativeWorkSeries
  */
-class BookSeries extends CreativeWorkSeries
+class BookSeries
 {
 }
