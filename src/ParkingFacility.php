f1c7f7543583c1cd76682ea444be7c31b27989b9
Tom Witkowski <dev.gummibeer@gmail.com>
2018-05-31 09:24:45 +0200

    regenerate with multiple parents src/ParkingFacility.php

--- src/ParkingFacility.php
+++ src/ParkingFacility.php
@@ -6,7 +6,9 @@ namespace Spatie\SchemaOrg;
  * A parking lot or other parking facility.
  *
  * @see http://schema.org/ParkingFacility
+ *
+ * @mixin CivicStructure
  */
-class ParkingFacility extends CivicStructure
+class ParkingFacility
 {
 }
