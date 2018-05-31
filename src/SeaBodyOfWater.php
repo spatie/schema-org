1795a552c6596d7ae71815b7cbb2c61e2ac4ac6e
Tom Witkowski <dev.gummibeer@gmail.com>
2018-05-31 09:25:02 +0200

    regenerate with multiple parents src/SeaBodyOfWater.php

--- src/SeaBodyOfWater.php
+++ src/SeaBodyOfWater.php
@@ -6,7 +6,9 @@ namespace Spatie\SchemaOrg;
  * A sea (for example, the Caspian sea).
  *
  * @see http://schema.org/SeaBodyOfWater
+ *
+ * @mixin BodyOfWater
  */
-class SeaBodyOfWater extends BodyOfWater
+class SeaBodyOfWater
 {
 }
