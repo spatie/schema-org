d78ea9e1523dc702aa960fe05fc4bb12877e0c93
Tom Witkowski <dev.gummibeer@gmail.com>
2018-05-31 09:24:23 +0200

    regenerate with multiple parents src/HousePainter.php

--- src/HousePainter.php
+++ src/HousePainter.php
@@ -6,7 +6,9 @@ namespace Spatie\SchemaOrg;
  * A house painting service.
  *
  * @see http://schema.org/HousePainter
+ *
+ * @mixin HomeAndConstructionBusiness
  */
-class HousePainter extends HomeAndConstructionBusiness
+class HousePainter
 {
 }
