24b143196088d0d4c92717cafdb0af01fad6de19
Tom Witkowski <dev.gummibeer@gmail.com>
2018-05-31 09:02:02 +0200

    update src/LocalBusiness.php

--- src/LocalBusiness.php
+++ src/LocalBusiness.php
@@ -27,8 +27,15 @@ class LocalBusiness extends Organization
     }
 
     /**
-     * The currency accepted (in [ISO 4217 currency
-     * format](http://en.wikipedia.org/wiki/ISO_4217)).
+     * The currency accepted.
+     * 
+     * Use standard formats: [ISO 4217 currency
+     * format](http://en.wikipedia.org/wiki/ISO_4217) e.g. "USD"; [Ticker
+     * symbol](https://en.wikipedia.org/wiki/List_of_cryptocurrencies) for
+     * cryptocurrencies e.g. "BTC"; well known names for [Local Exchange
+     * Tradings
+     * Systems](https://en.wikipedia.org/wiki/Local_exchange_trading_system)
+     * (LETS) and other currency types e.g. "Ithaca HOUR".
      *
      * @param string|string[] $currenciesAccepted
      *
@@ -71,7 +78,7 @@ class LocalBusiness extends Organization
     }
 
     /**
-     * Cash, credit card, etc.
+     * Cash, Credit Card, Cryptocurrency, Local Exchange Tradings System, etc.
      *
      * @param string|string[] $paymentAccepted
      *
