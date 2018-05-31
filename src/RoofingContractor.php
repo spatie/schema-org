85e97c0962f439f65e8c73f5b23faf2a51e9fb9c
Tom Witkowski <dev.gummibeer@gmail.com>
2018-05-31 09:25:00 +0200

    regenerate with multiple parents src/RoofingContractor.php

--- src/RoofingContractor.php
+++ src/RoofingContractor.php
@@ -6,7 +6,9 @@ namespace Spatie\SchemaOrg;
  * A roofing contractor.
  *
  * @see http://schema.org/RoofingContractor
+ *
+ * @mixin HomeAndConstructionBusiness
  */
-class RoofingContractor extends HomeAndConstructionBusiness
+class RoofingContractor
 {
 }
