10679de3165bde3afa6951c809bdf934742a2346
Tom Witkowski <dev.gummibeer@gmail.com>
2018-05-31 09:24:52 +0200

    regenerate with multiple parents src/PropertyValueSpecification.php

--- src/PropertyValueSpecification.php
+++ src/PropertyValueSpecification.php
@@ -6,8 +6,10 @@ namespace Spatie\SchemaOrg;
  * A Property value specification.
  *
  * @see http://schema.org/PropertyValueSpecification
+ *
+ * @mixin Intangible
  */
-class PropertyValueSpecification extends Intangible
+class PropertyValueSpecification
 {
     /**
      * The default value of the input.  For properties that expect a literal,
