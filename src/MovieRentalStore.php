43bf2e5a1764e65add9329f0d9aa6715b1e71d8c
Tom Witkowski <dev.gummibeer@gmail.com>
2018-05-31 09:24:37 +0200

    regenerate with multiple parents src/MovieRentalStore.php

--- src/MovieRentalStore.php
+++ src/MovieRentalStore.php
@@ -6,7 +6,9 @@ namespace Spatie\SchemaOrg;
  * A movie rental store.
  *
  * @see http://schema.org/MovieRentalStore
+ *
+ * @mixin Store
  */
-class MovieRentalStore extends Store
+class MovieRentalStore
 {
 }
