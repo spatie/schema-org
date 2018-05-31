92b54ce1c29d3c18634cdd1d5015821203c394d0
Tom Witkowski <dev.gummibeer@gmail.com>
2018-05-31 09:23:41 +0200

    regenerate with multiple parents src/AssignAction.php

--- src/AssignAction.php
+++ src/AssignAction.php
@@ -7,7 +7,9 @@ namespace Spatie\SchemaOrg;
  * something).
  *
  * @see http://schema.org/AssignAction
+ *
+ * @mixin AllocateAction
  */
-class AssignAction extends AllocateAction
+class AssignAction
 {
 }
