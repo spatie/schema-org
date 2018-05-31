9e39eb973bc42724558d2a0f25d55e48748a3d73
Tom Witkowski <dev.gummibeer@gmail.com>
2018-05-31 09:23:51 +0200

    regenerate with multiple parents src/Car.php

--- src/Car.php
+++ src/Car.php
@@ -6,7 +6,9 @@ namespace Spatie\SchemaOrg;
  * A car is a wheeled, self-powered motor vehicle used for transportation.
  *
  * @see http://schema.org/Car
+ *
+ * @mixin Vehicle
  */
-class Car extends Vehicle
+class Car
 {
 }
