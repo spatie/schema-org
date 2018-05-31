4dc92a5df1cae8002daa30c69a92a3eca8cf16ab
Tom Witkowski <dev.gummibeer@gmail.com>
2018-05-31 09:24:23 +0200

    regenerate with multiple parents src/IceCreamShop.php

--- src/IceCreamShop.php
+++ src/IceCreamShop.php
@@ -6,7 +6,9 @@ namespace Spatie\SchemaOrg;
  * An ice cream shop.
  *
  * @see http://schema.org/IceCreamShop
+ *
+ * @mixin FoodEstablishment
  */
-class IceCreamShop extends FoodEstablishment
+class IceCreamShop
 {
 }
