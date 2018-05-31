1b9a02af8c20eda5a2a2691061369d8a4527c6aa
Tom Witkowski <dev.gummibeer@gmail.com>
2018-05-31 09:24:36 +0200

    regenerate with multiple parents src/Mountain.php

--- src/Mountain.php
+++ src/Mountain.php
@@ -6,7 +6,9 @@ namespace Spatie\SchemaOrg;
  * A mountain, like Mount Whitney or Mount Everest.
  *
  * @see http://schema.org/Mountain
+ *
+ * @mixin Landform
  */
-class Mountain extends Landform
+class Mountain
 {
 }
