654332c255c3155764fc3dff1e9fea46eb5db41f
Tom Witkowski <dev.gummibeer@gmail.com>
2018-05-31 09:23:45 +0200

    regenerate with multiple parents src/BodyOfWater.php

--- src/BodyOfWater.php
+++ src/BodyOfWater.php
@@ -6,7 +6,9 @@ namespace Spatie\SchemaOrg;
  * A body of water, such as a sea, ocean, or lake.
  *
  * @see http://schema.org/BodyOfWater
+ *
+ * @mixin Landform
  */
-class BodyOfWater extends Landform
+class BodyOfWater
 {
 }
