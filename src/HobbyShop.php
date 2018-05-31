474ee9b35dbc4226bd62c37060274b813638081f
Tom Witkowski <dev.gummibeer@gmail.com>
2018-05-31 09:24:22 +0200

    regenerate with multiple parents src/HobbyShop.php

--- src/HobbyShop.php
+++ src/HobbyShop.php
@@ -6,7 +6,9 @@ namespace Spatie\SchemaOrg;
  * A store that sells materials useful or necessary for various hobbies.
  *
  * @see http://schema.org/HobbyShop
+ *
+ * @mixin Store
  */
-class HobbyShop extends Store
+class HobbyShop
 {
 }
