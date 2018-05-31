cb12dcdcf9955b4057119abbc8863121c9329fa7
Tom Witkowski <dev.gummibeer@gmail.com>
2018-05-31 09:24:19 +0200

    regenerate with multiple parents src/GovernmentOrganization.php

--- src/GovernmentOrganization.php
+++ src/GovernmentOrganization.php
@@ -6,7 +6,9 @@ namespace Spatie\SchemaOrg;
  * A governmental organization or agency.
  *
  * @see http://schema.org/GovernmentOrganization
+ *
+ * @mixin Organization
  */
-class GovernmentOrganization extends Organization
+class GovernmentOrganization
 {
 }
