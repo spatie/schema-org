6de195d8695b0186850c16f4230376ce932e4d2b
Tom Witkowski <dev.gummibeer@gmail.com>
2018-05-31 09:24:15 +0200

    regenerate with multiple parents src/FireStation.php

--- src/FireStation.php
+++ src/FireStation.php
@@ -6,7 +6,10 @@ namespace Spatie\SchemaOrg;
  * A fire station. With firemen.
  *
  * @see http://schema.org/FireStation
+ *
+ * @mixin CivicStructure
+ * @mixin EmergencyService
  */
-class FireStation extends CivicStructure
+class FireStation
 {
 }
