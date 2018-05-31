07416499ebb6778b6e226a842a8cc9b30e0f3cb6
Tom Witkowski <dev.gummibeer@gmail.com>
2018-05-31 09:23:42 +0200

    regenerate with multiple parents src/AutoRepair.php

--- src/AutoRepair.php
+++ src/AutoRepair.php
@@ -6,7 +6,9 @@ namespace Spatie\SchemaOrg;
  * Car repair business.
  *
  * @see http://schema.org/AutoRepair
+ *
+ * @mixin AutomotiveBusiness
  */
-class AutoRepair extends AutomotiveBusiness
+class AutoRepair
 {
 }
