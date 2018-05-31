de58b7a389beae4e0d7deb58bd23ebeab19aa779
Tom Witkowski <dev.gummibeer@gmail.com>
2018-05-31 09:23:52 +0200

    regenerate with multiple parents src/Casino.php

--- src/Casino.php
+++ src/Casino.php
@@ -6,7 +6,9 @@ namespace Spatie\SchemaOrg;
  * A casino.
  *
  * @see http://schema.org/Casino
+ *
+ * @mixin EntertainmentBusiness
  */
-class Casino extends EntertainmentBusiness
+class Casino
 {
 }
