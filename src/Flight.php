18a45a87f6cbd4183eecb42d810f27e422d27088
Tom Witkowski <dev.gummibeer@gmail.com>
2018-05-31 09:24:15 +0200

    regenerate with multiple parents src/Flight.php

--- src/Flight.php
+++ src/Flight.php
@@ -6,8 +6,10 @@ namespace Spatie\SchemaOrg;
  * An airline flight.
  *
  * @see http://schema.org/Flight
+ *
+ * @mixin Intangible
  */
-class Flight extends Intangible
+class Flight
 {
     /**
      * The kind of aircraft (e.g., "Boeing 747").
