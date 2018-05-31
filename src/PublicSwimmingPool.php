f996f285395a794269876a3dc2701079aa118cd9
Tom Witkowski <dev.gummibeer@gmail.com>
2018-05-31 09:24:52 +0200

    regenerate with multiple parents src/PublicSwimmingPool.php

--- src/PublicSwimmingPool.php
+++ src/PublicSwimmingPool.php
@@ -6,7 +6,9 @@ namespace Spatie\SchemaOrg;
  * A public swimming pool.
  *
  * @see http://schema.org/PublicSwimmingPool
+ *
+ * @mixin SportsActivityLocation
  */
-class PublicSwimmingPool extends SportsActivityLocation
+class PublicSwimmingPool
 {
 }
