88aabc714e8acbac88b998aeb9257802689f287a
Tom Witkowski <dev.gummibeer@gmail.com>
2018-05-31 09:24:54 +0200

    regenerate with multiple parents src/RadioClip.php

--- src/RadioClip.php
+++ src/RadioClip.php
@@ -6,7 +6,9 @@ namespace Spatie\SchemaOrg;
  * A short radio program or a segment/part of a radio program.
  *
  * @see http://schema.org/RadioClip
+ *
+ * @mixin Clip
  */
-class RadioClip extends Clip
+class RadioClip
 {
 }
