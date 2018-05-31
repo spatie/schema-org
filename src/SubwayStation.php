26230514b5a4342fd8c24252bb659f9d0a0cdc0d
Tom Witkowski <dev.gummibeer@gmail.com>
2018-05-31 09:25:09 +0200

    regenerate with multiple parents src/SubwayStation.php

--- src/SubwayStation.php
+++ src/SubwayStation.php
@@ -6,7 +6,9 @@ namespace Spatie\SchemaOrg;
  * A subway station.
  *
  * @see http://schema.org/SubwayStation
+ *
+ * @mixin CivicStructure
  */
-class SubwayStation extends CivicStructure
+class SubwayStation
 {
 }
