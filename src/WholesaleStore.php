b260e91d9ed147fa71b1d51eb07016a8b41b287b
Tom Witkowski <dev.gummibeer@gmail.com>
2018-05-31 09:25:25 +0200

    regenerate with multiple parents src/WholesaleStore.php

--- src/WholesaleStore.php
+++ src/WholesaleStore.php
@@ -6,7 +6,9 @@ namespace Spatie\SchemaOrg;
  * A wholesale store.
  *
  * @see http://schema.org/WholesaleStore
+ *
+ * @mixin Store
  */
-class WholesaleStore extends Store
+class WholesaleStore
 {
 }
