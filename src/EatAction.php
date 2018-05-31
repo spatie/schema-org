4f30cc1f378549a0bc2bfff504723163ddc5ba3e
Tom Witkowski <dev.gummibeer@gmail.com>
2018-05-31 09:24:09 +0200

    regenerate with multiple parents src/EatAction.php

--- src/EatAction.php
+++ src/EatAction.php
@@ -6,7 +6,9 @@ namespace Spatie\SchemaOrg;
  * The act of swallowing solid objects.
  *
  * @see http://schema.org/EatAction
+ *
+ * @mixin ConsumeAction
  */
-class EatAction extends ConsumeAction
+class EatAction
 {
 }
