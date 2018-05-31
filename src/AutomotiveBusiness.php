59dd3ac05f3a5adf8a6076ab8a38c0aa08f1abbc
Tom Witkowski <dev.gummibeer@gmail.com>
2018-05-31 09:23:43 +0200

    regenerate with multiple parents src/AutomotiveBusiness.php

--- src/AutomotiveBusiness.php
+++ src/AutomotiveBusiness.php
@@ -6,7 +6,9 @@ namespace Spatie\SchemaOrg;
  * Car repair, sales, or parts.
  *
  * @see http://schema.org/AutomotiveBusiness
+ *
+ * @mixin LocalBusiness
  */
-class AutomotiveBusiness extends LocalBusiness
+class AutomotiveBusiness
 {
 }
