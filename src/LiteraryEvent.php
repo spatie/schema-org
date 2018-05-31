ee235d33662ed1683d20fcca295c7abaa6679808
Tom Witkowski <dev.gummibeer@gmail.com>
2018-05-31 09:24:30 +0200

    regenerate with multiple parents src/LiteraryEvent.php

--- src/LiteraryEvent.php
+++ src/LiteraryEvent.php
@@ -6,7 +6,9 @@ namespace Spatie\SchemaOrg;
  * Event type: Literary event.
  *
  * @see http://schema.org/LiteraryEvent
+ *
+ * @mixin Event
  */
-class LiteraryEvent extends Event
+class LiteraryEvent
 {
 }
