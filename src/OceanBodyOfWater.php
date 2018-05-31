831ca7003c41081f633bb9792904ba7c408e11cc
Tom Witkowski <dev.gummibeer@gmail.com>
2018-05-31 09:24:41 +0200

    regenerate with multiple parents src/OceanBodyOfWater.php

--- src/OceanBodyOfWater.php
+++ src/OceanBodyOfWater.php
@@ -6,7 +6,9 @@ namespace Spatie\SchemaOrg;
  * An ocean (for example, the Pacific).
  *
  * @see http://schema.org/OceanBodyOfWater
+ *
+ * @mixin BodyOfWater
  */
-class OceanBodyOfWater extends BodyOfWater
+class OceanBodyOfWater
 {
 }
