c41163501b28792a7b259c85e995389bcba3e8db
Tom Witkowski <dev.gummibeer@gmail.com>
2018-05-31 09:24:51 +0200

    regenerate with multiple parents src/PresentationDigitalDocument.php

--- src/PresentationDigitalDocument.php
+++ src/PresentationDigitalDocument.php
@@ -6,7 +6,9 @@ namespace Spatie\SchemaOrg;
  * A file containing slides or used for a presentation.
  *
  * @see http://schema.org/PresentationDigitalDocument
+ *
+ * @mixin DigitalDocument
  */
-class PresentationDigitalDocument extends DigitalDocument
+class PresentationDigitalDocument
 {
 }
