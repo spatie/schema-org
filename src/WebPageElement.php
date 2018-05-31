915111176e0cf042ed5d87db240d53598e205499
Tom Witkowski <dev.gummibeer@gmail.com>
2018-05-31 09:25:25 +0200

    regenerate with multiple parents src/WebPageElement.php

--- src/WebPageElement.php
+++ src/WebPageElement.php
@@ -6,7 +6,9 @@ namespace Spatie\SchemaOrg;
  * A web page element, like a table or an image.
  *
  * @see http://schema.org/WebPageElement
+ *
+ * @mixin CreativeWork
  */
-class WebPageElement extends CreativeWork
+class WebPageElement
 {
 }
