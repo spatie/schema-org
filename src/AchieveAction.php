c6359aa266ff1586726122dbe2081ff63e8089e5
Tom Witkowski <dev.gummibeer@gmail.com>
2018-05-31 09:23:36 +0200

    regenerate with multiple parents src/AchieveAction.php

--- src/AchieveAction.php
+++ src/AchieveAction.php
@@ -7,7 +7,9 @@ namespace Spatie\SchemaOrg;
  * instantaneous action rather than an ongoing process.
  *
  * @see http://schema.org/AchieveAction
+ *
+ * @mixin Action
  */
-class AchieveAction extends Action
+class AchieveAction
 {
 }
