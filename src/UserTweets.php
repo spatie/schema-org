3c8a5ee805956992c45930e4a83537c55ecf814f
Tom Witkowski <dev.gummibeer@gmail.com>
2018-05-31 09:25:18 +0200

    regenerate with multiple parents src/UserTweets.php

--- src/UserTweets.php
+++ src/UserTweets.php
@@ -8,7 +8,9 @@ namespace Spatie\SchemaOrg;
  * vocabulary, alongside types such as [[Comment]].
  *
  * @see http://schema.org/UserTweets
+ *
+ * @mixin UserInteraction
  */
-class UserTweets extends UserInteraction
+class UserTweets
 {
 }
