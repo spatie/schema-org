6a397f81c18466ed5a35f76747226c330bf3a2ba
Tom Witkowski <dev.gummibeer@gmail.com>
2018-05-31 09:24:49 +0200

    regenerate with multiple parents src/Playground.php

--- src/Playground.php
+++ src/Playground.php
@@ -6,7 +6,9 @@ namespace Spatie\SchemaOrg;
  * A playground.
  *
  * @see http://schema.org/Playground
+ *
+ * @mixin CivicStructure
  */
-class Playground extends CivicStructure
+class Playground
 {
 }
