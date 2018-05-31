a0939de5a8f40836a39b0ca0dead56f88a3a5025
Tom Witkowski <dev.gummibeer@gmail.com>
2018-05-31 09:25:00 +0200

    regenerate with multiple parents src/RiverBodyOfWater.php

--- src/RiverBodyOfWater.php
+++ src/RiverBodyOfWater.php
@@ -6,7 +6,9 @@ namespace Spatie\SchemaOrg;
  * A river (for example, the broad majestic Shannon).
  *
  * @see http://schema.org/RiverBodyOfWater
+ *
+ * @mixin BodyOfWater
  */
-class RiverBodyOfWater extends BodyOfWater
+class RiverBodyOfWater
 {
 }
