ba3a884bdd2ee2f64d072677025d41f7631af72e
Tom Witkowski <dev.gummibeer@gmail.com>
2018-05-31 09:25:24 +0200

    regenerate with multiple parents src/WebApplication.php

--- src/WebApplication.php
+++ src/WebApplication.php
@@ -6,8 +6,10 @@ namespace Spatie\SchemaOrg;
  * Web applications.
  *
  * @see http://schema.org/WebApplication
+ *
+ * @mixin SoftwareApplication
  */
-class WebApplication extends SoftwareApplication
+class WebApplication
 {
     /**
      * Specifies browser requirements in human-readable text. For example,
