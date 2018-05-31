41c1669a6fd6625327026df46006415ba0f6d0dd
Tom Witkowski <dev.gummibeer@gmail.com>
2018-05-31 09:24:18 +0200

    regenerate with multiple parents src/GasStation.php

--- src/GasStation.php
+++ src/GasStation.php
@@ -6,7 +6,9 @@ namespace Spatie\SchemaOrg;
  * A gas station.
  *
  * @see http://schema.org/GasStation
+ *
+ * @mixin AutomotiveBusiness
  */
-class GasStation extends AutomotiveBusiness
+class GasStation
 {
 }
