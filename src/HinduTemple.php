931076dafadbd5c1689c27bc03723ba1129e1e57
Tom Witkowski <dev.gummibeer@gmail.com>
2018-05-31 09:24:22 +0200

    regenerate with multiple parents src/HinduTemple.php

--- src/HinduTemple.php
+++ src/HinduTemple.php
@@ -6,7 +6,9 @@ namespace Spatie\SchemaOrg;
  * A Hindu temple.
  *
  * @see http://schema.org/HinduTemple
+ *
+ * @mixin PlaceOfWorship
  */
-class HinduTemple extends PlaceOfWorship
+class HinduTemple
 {
 }
