9286a2af40505df14d752ad69bab1c43ca92af4b
Tom Witkowski <dev.gummibeer@gmail.com>
2018-05-31 09:24:02 +0200

    regenerate with multiple parents src/CurrencyConversionService.php

--- src/CurrencyConversionService.php
+++ src/CurrencyConversionService.php
@@ -6,7 +6,9 @@ namespace Spatie\SchemaOrg;
  * A service to convert funds from one currency to another currency.
  *
  * @see http://schema.org/CurrencyConversionService
+ *
+ * @mixin FinancialProduct
  */
-class CurrencyConversionService extends FinancialProduct
+class CurrencyConversionService
 {
 }
