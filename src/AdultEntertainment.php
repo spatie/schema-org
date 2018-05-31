301cdb1f00f073306bc318cfaf9d3bfffd86b54e
Tom Witkowski <dev.gummibeer@gmail.com>
2018-05-31 09:23:37 +0200

    regenerate with multiple parents src/AdultEntertainment.php

--- src/AdultEntertainment.php
+++ src/AdultEntertainment.php
@@ -6,7 +6,9 @@ namespace Spatie\SchemaOrg;
  * An adult entertainment establishment.
  *
  * @see http://schema.org/AdultEntertainment
+ *
+ * @mixin EntertainmentBusiness
  */
-class AdultEntertainment extends EntertainmentBusiness
+class AdultEntertainment
 {
 }
