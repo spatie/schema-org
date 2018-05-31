cef7fedbf22e5e7aeaaf77b18230660611f41b68
Tom Witkowski <dev.gummibeer@gmail.com>
2018-05-31 09:25:19 +0200

    regenerate with multiple parents src/VideoGameClip.php

--- src/VideoGameClip.php
+++ src/VideoGameClip.php
@@ -6,7 +6,9 @@ namespace Spatie\SchemaOrg;
  * A short segment/part of a video game.
  *
  * @see http://schema.org/VideoGameClip
+ *
+ * @mixin Clip
  */
-class VideoGameClip extends Clip
+class VideoGameClip
 {
 }
