09d36f870b92fbbcc86122f4defe4075a537c995
Tom Witkowski <dev.gummibeer@gmail.com>
2018-05-31 09:24:06 +0200

    regenerate with multiple parents src/Dentist.php

--- src/Dentist.php
+++ src/Dentist.php
@@ -6,7 +6,10 @@ namespace Spatie\SchemaOrg;
  * A dentist.
  *
  * @see http://schema.org/Dentist
+ *
+ * @mixin MedicalOrganization
+ * @mixin LocalBusiness
  */
-class Dentist extends MedicalOrganization
+class Dentist
 {
 }
