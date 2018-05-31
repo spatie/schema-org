bb9ffdc5823502778ac1372be59d1315979aad57
Tom Witkowski <dev.gummibeer@gmail.com>
2018-05-31 09:24:22 +0200

    regenerate with multiple parents src/Hospital.php

--- src/Hospital.php
+++ src/Hospital.php
@@ -6,7 +6,11 @@ namespace Spatie\SchemaOrg;
  * A hospital.
  *
  * @see http://schema.org/Hospital
+ *
+ * @mixin CivicStructure
+ * @mixin EmergencyService
+ * @mixin MedicalOrganization
  */
-class Hospital extends CivicStructure
+class Hospital
 {
 }
