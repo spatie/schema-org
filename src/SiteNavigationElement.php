01d2dc4296f55bd56e0cfd880d4599bd2c79f559
Tom Witkowski <dev.gummibeer@gmail.com>
2018-05-31 09:25:05 +0200

    regenerate with multiple parents src/SiteNavigationElement.php

--- src/SiteNavigationElement.php
+++ src/SiteNavigationElement.php
@@ -6,7 +6,9 @@ namespace Spatie\SchemaOrg;
  * A navigation element of the page.
  *
  * @see http://schema.org/SiteNavigationElement
+ *
+ * @mixin WebPageElement
  */
-class SiteNavigationElement extends WebPageElement
+class SiteNavigationElement
 {
 }
