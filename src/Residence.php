e3a613ba97fc1c0d711ec5ea88ea79ff71e57579
Tom Witkowski <dev.gummibeer@gmail.com>
2018-05-31 09:24:59 +0200

    regenerate with multiple parents src/Residence.php

--- src/Residence.php
+++ src/Residence.php
@@ -6,7 +6,9 @@ namespace Spatie\SchemaOrg;
  * The place where a person lives.
  *
  * @see http://schema.org/Residence
+ *
+ * @mixin Place
  */
-class Residence extends Place
+class Residence
 {
 }
