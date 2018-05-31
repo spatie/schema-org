26090d97361e9d0437afb55132552efcc49dfc4a
Tom Witkowski <dev.gummibeer@gmail.com>
2018-05-31 09:25:17 +0200

    regenerate with multiple parents src/UseAction.php

--- src/UseAction.php
+++ src/UseAction.php
@@ -6,7 +6,9 @@ namespace Spatie\SchemaOrg;
  * The act of applying an object to its intended purpose.
  *
  * @see http://schema.org/UseAction
+ *
+ * @mixin ConsumeAction
  */
-class UseAction extends ConsumeAction
+class UseAction
 {
 }
