04be8785af298df8459576d42435a961fc89ae7d
Tom Witkowski <dev.gummibeer@gmail.com>
2018-05-31 09:24:41 +0200

    regenerate with multiple parents src/NoteDigitalDocument.php

--- src/NoteDigitalDocument.php
+++ src/NoteDigitalDocument.php
@@ -6,7 +6,9 @@ namespace Spatie\SchemaOrg;
  * A file containing a note, primarily for the author.
  *
  * @see http://schema.org/NoteDigitalDocument
+ *
+ * @mixin DigitalDocument
  */
-class NoteDigitalDocument extends DigitalDocument
+class NoteDigitalDocument
 {
 }
