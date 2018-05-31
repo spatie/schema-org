7c5b19b67f18eb0bc39a1225948dda027df412fd
Tom Witkowski <dev.gummibeer@gmail.com>
2018-05-31 09:24:21 +0200

    regenerate with multiple parents src/HairSalon.php

--- src/HairSalon.php
+++ src/HairSalon.php
@@ -6,7 +6,9 @@ namespace Spatie\SchemaOrg;
  * A hair salon.
  *
  * @see http://schema.org/HairSalon
+ *
+ * @mixin HealthAndBeautyBusiness
  */
-class HairSalon extends HealthAndBeautyBusiness
+class HairSalon
 {
 }
