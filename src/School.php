ca209ad8cacfc61d3bf73878671d4d34f088e153
Tom Witkowski <dev.gummibeer@gmail.com>
2018-05-31 09:25:02 +0200

    regenerate with multiple parents src/School.php

--- src/School.php
+++ src/School.php
@@ -6,7 +6,9 @@ namespace Spatie\SchemaOrg;
  * A school.
  *
  * @see http://schema.org/School
+ *
+ * @mixin EducationalOrganization
  */
-class School extends EducationalOrganization
+class School
 {
 }
