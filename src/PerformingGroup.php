8dd44f1056f686067b2e475a5219d68b71fcb37c
Tom Witkowski <dev.gummibeer@gmail.com>
2018-05-31 09:24:47 +0200

    regenerate with multiple parents src/PerformingGroup.php

--- src/PerformingGroup.php
+++ src/PerformingGroup.php
@@ -6,7 +6,9 @@ namespace Spatie\SchemaOrg;
  * A performance group, such as a band, an orchestra, or a circus.
  *
  * @see http://schema.org/PerformingGroup
+ *
+ * @mixin Organization
  */
-class PerformingGroup extends Organization
+class PerformingGroup
 {
 }
