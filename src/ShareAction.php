5b2b4e8654b52b21e7bd1d4484c3635c5efea758
Tom Witkowski <dev.gummibeer@gmail.com>
2018-05-31 09:25:04 +0200

    regenerate with multiple parents src/ShareAction.php

--- src/ShareAction.php
+++ src/ShareAction.php
@@ -6,7 +6,9 @@ namespace Spatie\SchemaOrg;
  * The act of distributing content to people for their amusement or edification.
  *
  * @see http://schema.org/ShareAction
+ *
+ * @mixin CommunicateAction
  */
-class ShareAction extends CommunicateAction
+class ShareAction
 {
 }
