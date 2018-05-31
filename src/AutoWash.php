01b0277188f1d6c66436aed5e01846ba3906d26a
Tom Witkowski <dev.gummibeer@gmail.com>
2018-05-31 09:23:42 +0200

    regenerate with multiple parents src/AutoWash.php

--- src/AutoWash.php
+++ src/AutoWash.php
@@ -6,7 +6,9 @@ namespace Spatie\SchemaOrg;
  * A car wash business.
  *
  * @see http://schema.org/AutoWash
+ *
+ * @mixin AutomotiveBusiness
  */
-class AutoWash extends AutomotiveBusiness
+class AutoWash
 {
 }
