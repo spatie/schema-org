2dcad031ff903fcc5c1ec20b1009d15e46477bce
Tom Witkowski <dev.gummibeer@gmail.com>
2018-05-31 09:02:00 +0200

    update src/Corporation.php

--- src/Corporation.php
+++ src/Corporation.php
@@ -13,7 +13,7 @@ class Corporation extends Organization
      * The exchange traded instrument associated with a Corporation object. The
      * tickerSymbol is expressed as an exchange and an instrument name separated
      * by a space character. For the exchange component of the tickerSymbol
-     * attribute, we reccommend using the controlled vocaulary of Market
+     * attribute, we recommend using the controlled vocabulary of Market
      * Identifier Codes (MIC) specified in ISO15022.
      *
      * @param string|string[] $tickerSymbol
