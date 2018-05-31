7cde189974fec12b95f3743bda4c85f70de8cf1f
Tom Witkowski <dev.gummibeer@gmail.com>
2018-05-31 09:24:20 +0200

    regenerate with multiple parents src/GovernmentService.php

--- src/GovernmentService.php
+++ src/GovernmentService.php
@@ -7,8 +7,10 @@ namespace Spatie\SchemaOrg;
  * benefits, etc.
  *
  * @see http://schema.org/GovernmentService
+ *
+ * @mixin Service
  */
-class GovernmentService extends Service
+class GovernmentService
 {
     /**
      * The operating organization, if different from the provider.  This enables
