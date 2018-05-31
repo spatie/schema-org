1b85a8e7672ccb20ce6a8d6601ad09991196ecdc
Tom Witkowski <dev.gummibeer@gmail.com>
2018-05-31 09:23:51 +0200

    regenerate with multiple parents src/Canal.php

--- src/Canal.php
+++ src/Canal.php
@@ -6,7 +6,9 @@ namespace Spatie\SchemaOrg;
  * A canal, like the Panama Canal.
  *
  * @see http://schema.org/Canal
+ *
+ * @mixin BodyOfWater
  */
-class Canal extends BodyOfWater
+class Canal
 {
 }
