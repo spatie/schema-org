e7841b021e60b850d3398f11ccb7ba325e7d82d9
Tom Witkowski <dev.gummibeer@gmail.com>
2018-06-01 09:34:09 +0200

    extend BaseType src/DonateAction.php

--- src/DonateAction.php
+++ src/DonateAction.php
@@ -10,7 +10,7 @@ namespace Spatie\SchemaOrg;
  *
  * @mixin TradeAction
  */
-class DonateAction
+class DonateAction extends BaseType
 {
     /**
      * A sub property of participant. The participant who is at the receiving
