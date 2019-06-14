63068fdfc62d2a9770a8cb2ac4e6209311b71dad
Gummibeer <dev.gummibeer@gmail.com>
2019-06-14 13:50:49 +0200

    fix update generate and tag retriever

--- update.sh
+++ update.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
 UTC_NOW=$(date -u +"%Y-%m-%d %T %Z")
-php ./generate.php generate
+php ./generate.php
+git add .
 
 if [[ `git status --porcelain` ]]; then
     git fetch --all
-    export LATEST_TAG=$(git describe --tags)
+    export LATEST_TAG=$(git tag --list | awk '/./{line=$0} END{print line}')
     NEW_TAG=$(php -r "\$v = getenv('LATEST_TAG'); \$vs = explode('.', \$v); \$vs[1]++; echo implode('.', \$vs);")
     echo "changes at $UTC_NOW - release as $NEW_TAG"
 
