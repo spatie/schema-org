7561e2ea5a801954e0aabf5ad31f5a57a9ce0991
Gummibeer <dev.gummibeer@gmail.com>
2019-11-12 11:50:38 +0100

    generate latest version

--- src/CampingPitch.php
+++ src/CampingPitch.php
@@ -10,7 +10,7 @@ use \Spatie\SchemaOrg\Contracts\ThingContract;
  * A [[CampingPitch]] is an individual place for overnight stay in the outdoors,
  * typically being part of a larger camping site, or [[Campground]].
  * 
- *       
+ * 
  * In British English a campsite, or campground, is an area, usually divided
  * into a number of pitches, where people can camp overnight using tents or
  * camper vans or caravans; this British English use of the word is synonymous
--- src/Person.php
+++ src/Person.php
@@ -390,9 +390,14 @@ class Person extends BaseType implements ThingContract
     }
 
     /**
-     * Gender of the person. While http://schema.org/Male and
+     * Gender of something, typically a [[Person]], but possibly also fictional
+     * characters, animals, etc. While http://schema.org/Male and
      * http://schema.org/Female may be used, text strings are also acceptable
-     * for people who do not identify as a binary gender.
+     * for people who do not identify as a binary gender. The [[gender]]
+     * property can also be used in an extended sense to cover e.g. the gender
+     * of sports teams. As with the gender of individuals, we do not try to
+     * enumerate all possibilities. A mixed-gender [[SportsTeam]] can be
+     * indicated with a text value of "Mixed".
      *
      * @param GenderType|GenderType[]|string|string[] $gender
      *
