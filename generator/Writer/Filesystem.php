c675acb40dc88691b225dcaa090512952350b05d
Dan Pock <self@danpock.me>
2021-10-03 23:01:29 -0400

    Update dependencies, refactor & generator
    This commit updates the composer deps for the generator, then it refactors the generator code for breakage.
    And finally I run the generator to get the expected results of 0 actual changes to files in src.

--- generator/Writer/Filesystem.php
+++ generator/Writer/Filesystem.php
@@ -2,7 +2,7 @@
 
 namespace Spatie\SchemaOrg\Generator\Writer;
 
-use League\Flysystem\Adapter\Local;
+use League\Flysystem\Local\LocalFilesystemAdapter;
 use League\Flysystem\Filesystem as Flysystem;
 use Spatie\SchemaOrg\Generator\Type;
 use Spatie\SchemaOrg\Generator\TypeCollection;
@@ -29,7 +29,7 @@ class Filesystem
 
     public function __construct(string $root)
     {
-        $adapter = new Local($root);
+        $adapter = new LocalFilesystemAdapter($root);
         $this->flysystem = new Flysystem($adapter);
 
         $this->contractTemplate = new Template('Contract.php.twig');
@@ -41,8 +41,8 @@ class Filesystem
 
     public function clear()
     {
-        $this->flysystem->deleteDir('src');
-        $this->flysystem->createDir('src');
+        $this->flysystem->deleteDirectory('src');
+        $this->flysystem->deleteDirectory('src');
     }
 
     public function cloneStaticFiles()
@@ -54,7 +54,7 @@ class Filesystem
                 continue;
             }
 
-            $this->flysystem->put(
+            $this->flysystem->write(
                 str_replace('generator/templates/static', 'src', $file['path']),
                 $this->flysystem->read($file['path'])
             );
@@ -63,12 +63,12 @@ class Filesystem
 
     public function createType(Type $type)
     {
-        $this->flysystem->put(
+        $this->flysystem->write(
             "src/Contracts/{$type->className}Contract.php",
             $this->contractTemplate->render(['type' => $type])
         );
 
-        $this->flysystem->put(
+        $this->flysystem->write(
             "src/{$type->className}.php",
             $this->typeTemplate->render(['type' => $type])
         );
@@ -76,17 +76,17 @@ class Filesystem
 
     public function createBuilderClass(TypeCollection $types)
     {
-        $this->flysystem->put(
+        $this->flysystem->write(
             'src/Schema.php',
             $this->builderClassTemplate->render(['types' => $types->toArray()])
         );
 
-        $this->flysystem->put(
+        $this->flysystem->write(
             'src/Graph.php',
             $this->graphClassTemplate->render(['types' => $types->toArray()])
         );
 
-        $this->flysystem->put(
+        $this->flysystem->write(
             'src/MultiTypedEntity.php',
             $this->multiTypedEntityClassTemplate->render(['types' => $types->toArray()])
         );
