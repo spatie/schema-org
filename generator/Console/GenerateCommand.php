c5ed25356b50773d723087792104304c92c64604
Dan Pock <self@danpock.me>
2021-10-03 23:12:14 -0400

    Update generator to use schema v10

--- generator/Console/GenerateCommand.php
+++ generator/Console/GenerateCommand.php
@@ -32,7 +32,7 @@ class GenerateCommand extends Command
         $generator = new PackageGenerator();
 
         $definitions = new Definitions([
-            'core' => 'https://raw.githubusercontent.com/schemaorg/schemaorg/main/data/releases/9.0/schemaorg-all-https.jsonld',
+            'core' => 'https://raw.githubusercontent.com/schemaorg/schemaorg/main/data/releases/10.0/schemaorg-all-https.jsonld',
         ]);
 
         if (! $input->getOption('local')) {
