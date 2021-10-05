68df460e839d856700d0cf73a8a590c3156c3587
Dan Pock <self@danpock.me>
2021-10-05 13:18:05 -0400

    Remove all the pre-processing attempts to conform data

--- generator/Parser/JsonLdParser.php
+++ generator/Parser/JsonLdParser.php
@@ -6,72 +6,12 @@ use Illuminate\Support\Collection;
 
 class JsonLdParser
 {
-    protected string $schemaPrefix;
     protected Collection $jsonLdGraph;
 
     public function __construct(string $rawJsonLd)
     {
         $parsedJson = json_decode($rawJsonLd, true);
-        $this->schemaPrefix = $parsedJson['@context']['schema'];
-        $this->jsonLdGraph = collect($parsedJson['@graph'])->map(function ($schemaArray) {
-            $schemaArray['@id'] = $this->replaceSchemaPrefix($schemaArray['@id']);
-            if (isset($schemaArray['rdfs:subClassOf'])) {
-                if (isset($schemaArray['rdfs:subClassOf']['@id'])) {
-                    $schemaArray['rdfs:subClassOf']['@id'] = $this->replaceSchemaPrefix(
-                        $schemaArray['rdfs:subClassOf']['@id']
-                    );
-                } else {
-                    $temp = [];
-                    foreach ($schemaArray['rdfs:subClassOf'] as $key => $value) {
-                        $value['@id'] = $this->replaceSchemaPrefix($value['@id']);
-                        $temp[] = $value;
-                    }
-                    $schemaArray['rdfs:subClassOf'] = $temp;
-                }
-            }
-            if (isset($schemaArray['schema:isPartOf'])) {
-                $schemaArray['schema:isPartOf']['@id'] = $this->replaceSchemaPrefix(
-                    $schemaArray['schema:isPartOf']['@id']
-                );
-            }
-
-            if (isset($schemaArray['schema:domainIncludes'])) {
-                if (isset($schemaArray['schema:domainIncludes']['@id'])) {
-                    $schemaArray['schema:domainIncludes']['@id'] = $this->replaceSchemaPrefix(
-                        $schemaArray['schema:domainIncludes']['@id']
-                    );
-                } else {
-                    $temp = [];
-                    foreach ($schemaArray['schema:domainIncludes'] as $key => $value) {
-                        $value['@id'] = $this->replaceSchemaPrefix($value['@id']);
-                        $temp[] = $value;
-                    }
-                    $schemaArray['schema:domainIncludes'] = $temp;
-                }
-            }
-
-            if (isset($schemaArray['schema:rangeIncludes'])) {
-                if (isset($schemaArray['schema:rangeIncludes']['@id'])) {
-                    $schemaArray['schema:rangeIncludes']['@id'] = $this->replaceSchemaPrefix(
-                        $schemaArray['schema:rangeIncludes']['@id']
-                    );
-                } else {
-                    $temp = [];
-                    foreach ($schemaArray['schema:rangeIncludes'] as $key => $value) {
-                        $value['@id'] = $this->replaceSchemaPrefix($value['@id']);
-                        $temp[] = $value;
-                    }
-                    $schemaArray['schema:rangeIncludes'] = $temp;
-                }
-            }
-
-            return $schemaArray;
-        });
-    }
-
-    private function replaceSchemaPrefix(string $attributeValue): string
-    {
-        return str_replace('schema:', $this->schemaPrefix, $attributeValue);
+        $this->jsonLdGraph = collect($parsedJson['@graph']);
     }
 
     public function filter(string $selector): Collection
