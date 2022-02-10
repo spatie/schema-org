26fa21e423d6be10890b525d5f2a62fd3149f507
Niels Vanpachtenbeke <10651054+Nielsvanpach@users.noreply.github.com>
2022-02-10 13:31:17 +0100

    remove phpdoc rules

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -17,14 +17,14 @@ return (new PhpCsFixer\Config())
         'no_unused_imports' => true,
         'not_operator_with_successor_space' => true,
         'trailing_comma_in_multiline' => true,
-        'phpdoc_scalar' => true,
+        //'phpdoc_scalar' => true,
         'unary_operator_spaces' => true,
         'binary_operator_spaces' => true,
         'blank_line_before_statement' => [
             'statements' => ['break', 'continue', 'declare', 'return', 'throw', 'try'],
         ],
-        'phpdoc_single_line_var_spacing' => true,
-        'phpdoc_var_without_name' => true,
+        //'phpdoc_single_line_var_spacing' => true,
+        //'phpdoc_var_without_name' => true,
         'class_attributes_separation' => [
             'elements' => [
                 'method' => 'one',
