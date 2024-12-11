4914c6bbca144f08099a75fc42b970d3d71b015c
Sören Wünsch <soeren.wrede@parshipgroup.com>
2024-12-11 15:14:59 +0100

    Upgrade to schema.org v28.1

---
+++ src/Contracts/MemberProgramContract.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Spatie\SchemaOrg\Contracts;
+
+interface MemberProgramContract
+{
+    public function additionalType($additionalType);
+
+    public function alternateName($alternateName);
+
+    public function description($description);
+
+    public function disambiguatingDescription($disambiguatingDescription);
+
+    public function hasTiers($hasTiers);
+
+    public function hostingOrganization($hostingOrganization);
+
+    public function identifier($identifier);
+
+    public function image($image);
+
+    public function mainEntityOfPage($mainEntityOfPage);
+
+    public function name($name);
+
+    public function potentialAction($potentialAction);
+
+    public function sameAs($sameAs);
+
+    public function subjectOf($subjectOf);
+
+    public function url($url);
+}
